988bb8b0972ba7f7da9747dcafb68155e426b116
srinivas5675 <srinivasaraokolli9@gmail.com>
2024-05-16 10:02:15 +0530

    navbar changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
 // App.js
+// import React from 'react';
+// import { BrowserRouter as Router, Routes ,Route} from 'react-router-dom';
+// import Dashboard from './Dashboard';
+// import About from './About';
+// import Home from './Home';
+// import Veeru from './Linking';
+
+
+// const App = () => {
+//     return (
+//         <Router>
+//             <Veeru/>
+//             <Routes>
+//                 <Route path="/" element={<Home/>} />
+//                 <Route path="/About" element={<About/>} />
+//                 <Route path="/Dashboard" element={<Dashboard/>} />
+//             </Routes>
+//         </Router>
+//     );
+// }
+
+// export default App;
+
 import React from 'react';
-import { BrowserRouter as Router, Routes ,Route} from 'react-router-dom';
-import Dashboard from './Dashboard';
-import About from './About';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './Navbar';
 import Home from './Home';
-import Veeru from './Linking';
+import About from './About';
+import Dashboard from './Dashboard';
 
-
-const App = () => {
-    return (
-        <Router>
-            <Veeru/>
-            <Routes>
-                <Route path="/" element={<Home/>} />
-                <Route path="/About" element={<About/>} />
-                <Route path="/Dashboard" element={<Dashboard/>} />
-            </Routes>
-        </Router>
-    );
+function App() {
+  return (
+    <Router>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/Navbar.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+
+export default function Navbar() {
+  return (
+    <Box sx={{ flexGrow: 3 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 5 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            News
+          </Typography>
+          <Button component={Link} to="/" color="inherit">Home</Button>
+          <Button component={Link} to="/about" color="inherit">About</Button>
+          <Button component={Link} to="/dashboard" color="inherit">Dashboard</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
